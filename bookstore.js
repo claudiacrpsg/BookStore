947d0b58117b5c83909378f6fe643532c2c04271
claudia <claudiacrpsantos@hotmail.com>
2019-03-03 08:31:58 +0100

    minor details changed

--- bookstore.js
+++ bookstore.js
@@ -1,46 +1,41 @@
-function eachBook (){
-        var bookCase = document.getElementById("bookCase");
-        var bookData = data.books;
-        bookCase.innerHTML = "";
-        var template = "";
-        for (var i = 0; i < bookData.length; i++) {
-            template += `  
-            <div class="flip-card">
-            <div class="flip-card-inner">
-                <div class="flip-card-front">
-                    <img src="${bookData[i].cover}" alt="">
-                </div>
-                <div class="flip-card-back">
-                    <h4 id="li">${bookData[i].title}</h4>
-                    <h5>${bookData[i].description}</h5>
-                    <a href="${bookData[i].detail}" data-lightbox="coverImage"><button type="button" class="btn btn-dark">More info</button></a>
-                </div>
-            </div>
-            </div>
-            `;
-        }
-        bookCase.innerHTML = template;
-  }
-eachBook();
+getData();
 
+function getData() {
+    fetch('https://api.myjson.com/bins/zyv02')
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            data = data;
+            console.log(data);
+            getBooks(data);
+        });
+}
 
-function bookSearch(){
-    var search = document.getElementById("mySearch");
-    var input = search.value;
-    console.log(input);
-
+function getBooks(data) {
+    var bookCase = document.getElementById("bookCase");
     var bookData = data.books;
-    console.log(bookData);
+    // bookCase.innerHTML = "";
+    var template = "";
+    for (var i = 0; i < bookData.length; i++) {
+        template += `  
+    <div class="flip-card" id="flipCard">
+    <div class="flip-card-inner">
+        <div class="flip-card-front">
+            <img src="${bookData[i].cover}" alt="">
+        </div>
+        <div class="flip-card-back">
+            <h4 id="bookTitle">${bookData[i].title}</h4>
+            <h5>${bookData[i].description}</h5>
+            <a href="${bookData[i].detail}" data-lightbox="coverImage"><button type="button" class="btn btn-dark">More info</button></a>
+        </div>
+    </div>
+    </div>
+    `;
+        bookCase.innerHTML = template;
 
-    var bookTitle = data.books[0].title;
-    console.log(bookTitle);
-
-    var empty = [];
-    for(var i = 0; i < bookTitle.length; i++){
-       
     }
 }
 
-bookSearch();
 
 
